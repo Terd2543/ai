cc646cff50fa326649b60f6459d293a6fabcaf98
Terd2543 <terdteykk@gmail.com>
2026-02-21 06:45:32 +0700

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,51 @@
+getgenv().Hermanos_Settings = {
+    ['key'] = 'cb01626e-306f-4587-8bd7-7c5cf39aca24',
+    ['PC'] = 'CHANGE-ME',
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/main.lua'))() end)
+
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Configs = {
+["Quest"] = {
+["Evo Race V1"] = true;
+["Evo Race V2"] = true;
+["RGB Haki"] = false;
+["Pull Lerver"] = true;
+};
+Sword = {
+"Dual-Headed Blade";
+"Smoke Admiral";
+"Wardens Sword";
+"Cutlass";
+"Katana";
+"Dual Katana";
+"Triple Katana";
+"Iron Mace";
+"Saber";
+"Pole (1st Form)";
+"Gravity Blade";
+"Longsword";
+"Rengoku";
+"Midnight Blade";
+"Soul Cane";
+"Bisento";
+"Yama";
+"Tushita";
+"Cursed Dual Katana";
+};
+Gun = {
+"Soul Guitar";
+"Kabucha";
+"Venom Bow";
+"Musket";
+"Flintlock";
+"Refined Slingshot";
+"Magma Blaster";
+"Dual Flintlock";
+"Cannon";
+"Bizarre Revolver";
+"Bazooka";
+};
+["FPS Booster"] = false;
+};
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/8cffffd967953fe7"))()
